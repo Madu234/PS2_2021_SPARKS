64691d8744fdb5e1c0931f33f321692319b11b97
Madu234 <79806642+Madu234@users.noreply.github.com>
2021-05-21 00:21:22 +0300

    Tema1

--- Arduino codes/1_Project/1_Project.ino
+++ Arduino codes/1_Project/1_Project.ino
@@ -1,11 +1,12 @@
 int val;
 int tempPin = 1;
-char bufer;
+String bufer;
 void setup()
 {
   Serial.begin(9600);
-  pinMode(6,OUTPUT);
-  
+  pinMode(5,OUTPUT); //Red
+  pinMode(6,OUTPUT); //Green
+  pinMode(7,OUTPUT); //Blue
 }
 void loop()
 {
@@ -18,10 +19,25 @@ void loop()
   delay(1000);
   if (Serial.available() > 0)
   {
-    bufer = Serial.read();
-    if (bufer == 'a')
+    bufer = Serial.readStringUntil('\n');
+    if (bufer[0] == 'a'){
+       analogWrite(7,0);
        analogWrite(6,0);
-    else if (bufer == 's')
-       analogWrite(6,255);
+       analogWrite(5,0);
+    }
+    else if (bufer[0] == 's'){
+      analogWrite(7,260);
+      analogWrite(6,270);
+      analogWrite(5,255);
+    }
+    else if (bufer[0] == 'p'){
+      int Red,Green,Blue;
+      Red = 255-10*(bufer[1]-48)-10*(bufer[2]-48); 
+      Green = 270-10*(bufer[3]-48)-10*(bufer[4]-48);
+      Blue = 260-10*(bufer[5]-48)-10*(bufer[6]-48);
+      analogWrite(7,Blue);
+      analogWrite(6,Green);
+      analogWrite(5,Red);
+    }
   }
 }
